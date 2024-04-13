10c808b58826107b0c67809e15f29a20e4887f2d
koheinthurako <captainthura99@gmail.com>
2024-04-14 01:43:13 +0630

    setup environment

---
+++ Backend/Backend/src/main/java/com/Havenly/Backend/BackendApplication.java
@@ -0,0 +1,19 @@
+package com.Havenly.Backend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Connected to Database!");
+	}
+
+}
