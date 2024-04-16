a879e85c14dc6d5ecdbd38b84bb662575851522e
Kinoko2146 <kinoko2146@gmail.com>
2024-04-16 23:49:16 +0630

    Recreated becoz can't run pulled project

---
+++ Frontend/havenly-frontend/havenly/src/components/mainIndexVue.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ title }}</h2>
+    <p>{{ sub_title }}</p>
+</template>
+
+<script>
+
+export default {
+    name: 'mainIndexVue',
+
+    data:()=> ({
+        title: 'Hello world',
+        sub_title: 'sleep time ;)'
+    }),
+}
+
+</script>
--- Frontend/havenly-frontend/havenly/src/router/index.js
+++ Frontend/havenly-frontend/havenly/src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeIndex from '../components/mainIndexVue.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/home',
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/Home',
+    redirect: '/home',
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: HomeIndex
+  },
+
 ]
 
 const router = createRouter({
