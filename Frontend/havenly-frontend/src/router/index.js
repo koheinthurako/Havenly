f80d43e6b3a306b99525fd9e0a6fa57cd2fbaa49
Aung Khant Min <aungkhantm33@gmail.com>
2024-04-19 18:21:55 +0700

    update

--- Frontend/havenly-frontend/src/router/index.js
+++ Frontend/havenly-frontend/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
   {
-    path: '/login',
-    name: 'LoginView',
-    component: LoginView
+    path: '/register',
+    name: 'Register',
+    component: Register
   },
   {
     path: '/about',
