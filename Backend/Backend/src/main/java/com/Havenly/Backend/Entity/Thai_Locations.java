8a611c71e4fddb3d8d82b713117cbfc4a5734c04
koheinthurako <captainthura99@gmail.com>
2024-04-19 20:54:03 +0630

    Add necessary files

--- Backend/Backend/src/main/java/com/Havenly/Backend/Entity/Thai_Locations.java
+++ Backend/Backend/src/main/java/com/Havenly/Backend/Entity/Thai_Locations.java
@@ -3,9 +3,17 @@ package com.Havenly.Backend.Entity;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @EnableJpaRepositories
 public class Thai_Locations {
+	
+	
 	
 }
