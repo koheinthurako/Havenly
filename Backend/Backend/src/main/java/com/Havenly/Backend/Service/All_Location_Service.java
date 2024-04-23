cd2980b7b94d55d2be08ae6d8f1436f09228bbcb
Aung Khant Min <aungkhantm33@gmail.com>
2024-04-23 18:15:09 +0700

    update

--- Backend/Backend/src/main/java/com/Havenly/Backend/Service/All_Location_Service.java
+++ Backend/Backend/src/main/java/com/Havenly/Backend/Service/All_Location_Service.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.Havenly.Backend.DTO.All_Location_DTO;
-import com.Havenly.Backend.Entity.All_Location;
 
 @Service
 public interface All_Location_Service {
